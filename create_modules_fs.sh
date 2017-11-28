ab68178decf05377c960aa662d1272270083a717
gnaghi <gnaghi@free.fr>
2017-11-28 17:18:28 +0100

    Added debugfs option. Don't forget to remove it for release version. Added Uevent helper option. Tweaked create_module_fs script to delete source and build directories.

--- create_modules_fs.sh
+++ create_modules_fs.sh
@@ -6,14 +6,18 @@ cd "$DIR"
 rm -rf /tmp/modules_fs
 mkdir /tmp/modules_fs
 INSTALL_MOD_PATH=/tmp/modules_fs ARCH=mips make modules_install
-
 NAME=`ls -d /tmp/modules_fs/lib/modules/*`
 NAME=`basename "$NAME" +`
 
 if [ -d  "/tmp/modules_fs/lib/modules/${NAME}" ] ; then
 	ln -s "${NAME}" "/tmp/modules_fs/lib/modules/${NAME}+"
+	rm "/tmp/modules_fs/lib/modules/${NAME}+/build"
+	rm "/tmp/modules_fs/lib/modules/${NAME}+/source"
 else
 	ln -s "${NAME}+" "/tmp/modules_fs/lib/modules/${NAME}"
+        rm "/tmp/modules_fs/lib/modules/${NAME}/build"
+        rm "/tmp/modules_fs/lib/modules/${NAME}/source"
 fi
+
 
 mksquashfs /tmp/modules_fs/lib/modules modules.squashfs -all-root -noappend -no-exports -no-xattrs
